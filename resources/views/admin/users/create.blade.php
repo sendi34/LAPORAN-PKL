344802340730ec26ee10126a677fcc31df28935c
sendi34 <sendipratama302@gmail.com>
2026-01-01 09:52:33 +0800

    memperbarui nama di pengguna

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.sbadmin')
-@section('title','Tambah User')
+@section('title', 'Tambah User')
 
 @section('content')
 
-<h1 class="h3 mb-4">Tambah User</h1>
-<div class="card shadow">
-    <div class="card-body">
-        <form method="POST" action="{{ route('admin.users.store') }}">
-            @csrf
-            @include('admin.users.form')
-            <button class="btn btn-success mt-3">Simpan</button>
-            <a href="{{ route('admin.users.index') }}" class="btn btn-secondary mt-3">
-                Batal
-            </a>
-        </form>
+    <h1 class="h3 mb-4">Tambah Pengguna</h1>
+    <div class="card shadow">
+        <div class="card-body">
+            <form method="POST" action="{{ route('admin.users.store') }}">
+                @csrf
+                @include('admin.users.form')
+                <button class="btn btn-success mt-3">Simpan</button>
+                <a href="{{ route('admin.users.index') }}" class="btn btn-secondary mt-3">
+                    Batal
+                </a>
+            </form>
+        </div>
     </div>
-</div>
 
 
 @endsection
